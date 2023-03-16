51218a1ee00045792dbb9ace36e7b41c4bfffb95
ShahidaAkter1 <shahida0200@gmail.com>
2023-03-16 09:40:33 +0600

    addEventLisener error solved

--- index.js
+++ index.js
@@ -74,7 +74,8 @@ const displayQuiz = (data) => {
 };
 
 // EventListener for quiz submit button
-document.querySelectorAll("#submit").addEventlistener("click", () => {
+document.querySelector('#submit').addEventListener('click', () =>{
+
   if (answers.length < 6) {
     return;
   }
